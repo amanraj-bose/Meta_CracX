a12a756cdb4fcbf59bf2a915e2031ec9b456ba95
amanraj-bose <88227750+amanraj-bose@users.noreply.github.com>
2022-11-06 15:22:06 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-import os
-import shutil
-import sys
-import subprocess
+from setuptools import setup, find_packages
+
+setup(
+    name="metacrax",
+    version="0.0.1",
+    packages=find_packages(),
+    author="amanraj-bose",
+    author_email="amanraj90770124@gmail.com",
+    install_requires=["scikit-learn", "joblib", "pyfiglet", "numpy", "pandas", "seaborn", "matplotlib", "python-nmap"],
+    description="It is allow you to hack the metasploitable 2 and it is only made up for beginners.",
+    url="https://github.com/amanraj-bose/metacrax",
+    entry_points = {"console_scripts": ['metacrax = tool:USER']},
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
+
+
